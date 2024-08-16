9e7e8855a063392161e03d1aece05095c1955df9
Pongsakan-fluk <pongsakan.obr@futurelifestyle.co.th>
2024-08-16 16:00:09 +0700

    Add our-partner

--- src/components/content/sections/Our-Partner.jsx
+++ src/components/content/sections/Our-Partner.jsx
@@ -4,6 +4,10 @@ function OurPartner() {
   return (
     <div className="w-3/4 flex justify-center mx-auto">
       <h1 className="text-cyan-700">Our Solution Partners</h1>
+
+      <div className="w-full flex flex-wrap">
+        <img src="" alt="" />
+      </div>
     </div>
   );
 }
